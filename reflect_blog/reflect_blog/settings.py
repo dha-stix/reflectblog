5f6c26e69d8e94290325a9339a92c849778906d9
dha-stix <asaoludavid234@yahoo.com>
2021-05-01 04:33:38 +0100

    Update settings.py

--- reflect_blog/reflect_blog/settings.py
+++ reflect_blog/reflect_blog/settings.py
@@ -11,12 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import environ
 import django_heroku
 import dj_database_url
 
-env = environ.Env()
-environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,11 +133,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-SECRET_KEY = env('SECRET_KEY')
-AWS_S3_CUSTOM_DOMAIN = env('AWS_LINK')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_LINK')
 
 AWS_LOCATION = 'static'
 
